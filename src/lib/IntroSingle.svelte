483f2d982cc88d7dc6fbdc06fa10e3fe210a83a8
Josué Cintra <josuecintra@gmail.com>
2025-10-16 15:52:45 -0300

    Componente IntroSingle

---
+++ src/lib/IntroSingle.svelte
@@ -0,0 +1,53 @@
+<script>
+    export let data;
+    console.log(data);
+</script>
+
+<div class="intro__wrapoer wrapper">
+    <div class="intro__content">
+        <h2>{data.name}</h2>
+        <p>{data.content}</p>
+    </div>
+    <div class="intro__img">
+        <img src="{data.img}" alt="{data.name}" />
+    </div>
+
+</div>
+
+<style>
+    .intro__wrapoer {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        gap: 2rem;
+        max-height: 360px;
+        align-items: center;
+    }
+
+    .intro__content {
+        
+    }
+
+    .intro__img {
+        margin-right: -20px;
+        position: relative;
+    }
+
+    .intro__img::before {
+        content: "";
+        display: block;
+        position: absolute;
+        inset: 0;
+        background-color: var(--overlay);
+    }
+
+    img {
+        border-radius: 0 0 0 8px;
+        height: 360px;
+        object-fit: cover;
+    }
+
+    p {
+        font-size: calc(18 / 16 * 1rem);
+        margin-top: 8px;
+    }
+</style>
--- src/routes/modalidades/[slug]/+page.svelte
+++ src/routes/modalidades/[slug]/+page.svelte
@@ -2,6 +2,8 @@
     import { page } from '$app/state';
     import { modalidades } from '$lib/stores';
 
+    import IntroSingle from '$lib/IntroSingle.svelte';
+
 	let modalidade = page.params.slug;
 
     let data = $modalidades.find(m => m.slug === modalidade);
@@ -10,4 +12,4 @@
 
 </script>
 
-<h1>Slug id = {modalidade}</h1>
+<IntroSingle {data} />
